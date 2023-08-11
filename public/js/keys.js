37fe781ed833fe1131707c0f59869f1bdf4c0e9c
AYUSSH ADARSSH SRIVASTAV <89251359+ayussh27@users.noreply.github.com>
2023-08-11 21:35:10 +0530

    Add files via upload

---
+++ public/js/keys.js
@@ -0,0 +1,5 @@
+const MAPBOX_KEY = 'pk.eyJ1Ijoic2F1bXlhcmFuamFuMTIzIiwiYSI6ImNrbWNhOTFxdTAwd3kyb3A1ejg4bWJvZzgifQ.ca-4KiendhcKMpkNx4DZLw';
+const OPEN_WEATHER_APPID = '6db1adb8691ea5db220273eb2876c16d';
+const mongodbw = 'mongodb+srv://Saumyaranjan:mongodb@cluster0.yqomn.mongodb.net/users'
+
+module.exports = mongodbw;
